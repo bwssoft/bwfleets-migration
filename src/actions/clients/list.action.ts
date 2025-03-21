0651bb796492c437214458daa8a7123f87e86246
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-21 15:43:24 -0300

    style: remove console

--- src/actions/clients/list.action.ts
+++ src/actions/clients/list.action.ts
@@ -5,7 +5,6 @@ interface ListAllClientsParams {
 }
 
 export async function listAllClients({ page = 1 }: ListAllClientsParams) {
-  console.log("🚀 ~ listAllClients ~ page:", page);
   return await prisma.client.findMany({
     skip: 100 * Number(page),
     take: 100,
