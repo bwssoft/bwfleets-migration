802428d4b39eaf5ec8a594ec1978270981890249
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-21 15:20:54 -0300

    feat: add pagination component usage on wwt client table

--- src/components/table/wwt-client.table.tsx
+++ src/components/table/wwt-client.table.tsx
@@ -3,8 +3,13 @@
 import { WWTClient } from "@/@shared/interfaces/wwt-client";
 import { DataTable } from "../ui/data-table";
 import { ColumnDef } from "@tanstack/react-table";
+import { DataTablePagination } from "../ui/data-table-pagination";
 
 const columns: Array<ColumnDef<WWTClient>> = [
+  {
+    accessorKey: "accountId",
+    header: "AccountID",
+  },
   {
     accessorKey: "accountName",
     header: "Nome do cliente",
@@ -32,5 +37,10 @@ interface WWTClientTableProps {
 }
 
 export function WWTClientTable({ data }: WWTClientTableProps) {
-  return <DataTable data={data} columns={columns} />;
+  return (
+    <section className="space-y-4">
+      <DataTable data={data} columns={columns} />
+      <DataTablePagination count={25000} pageSize={100} currentPage={1} />
+    </section>
+  );
 }
