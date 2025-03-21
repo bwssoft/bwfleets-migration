7182c7aca7b5f572be0a32a5d37180c01936a3c5
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-21 11:31:53 -0300

    feat: page title

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -4,7 +4,8 @@ import { SearchClientForm } from "../../components/forms/search-client.form";
 export default function ClientsPage() {
   return (
     <main className="w-screen h-screen container p-6 text-sm space-y-4">
-      <header>
+      <header className="flex items-center justify-between">
+        <h3 className="text-lg font-bold tracking-tight">Clientes</h3>
         <SearchClientForm />
       </header>
 
