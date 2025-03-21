2f46d3880b2cb75847127af637ee5b07e9bba4f6
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-21 12:30:48 -0300

    feat: add more components

---
+++ src/components/ui/contained-label-value.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+
+type ContainedLabelValueContainerProps = ComponentProps<"div">;
+
+function ContainedLabelValue({
+  className,
+  ...rest
+}: ContainedLabelValueContainerProps) {
+  return (
+    <div
+      className={cn(
+        "border border-border rounded-md shadow-sm bg-card divide-y divide-border",
+        className
+      )}
+      {...rest}
+    />
+  );
+}
+
+interface ContainedLabelValueProps {
+  label: string;
+  value?: any;
+}
+
+function ContainedLabelValueItem({ label, value }: ContainedLabelValueProps) {
+  return (
+    <div className="grid grid-cols-6 p-2.5 text-xs">
+      <span className="col-span-2 font-medium">{label}</span>
+      <span className="col-span-4">{value ?? "--"}</span>
+    </div>
+  );
+}
+
+export { ContainedLabelValue, ContainedLabelValueItem };
