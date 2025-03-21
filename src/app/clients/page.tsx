805c465b261a43095e5b7e5de7475f620d272caf
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-21 15:21:36 -0300

    feat: add find many usage on clients page

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,21 +1,38 @@
 import { WWTClientTable } from "@/components/table/wwt-client.table";
 import { SearchClientForm } from "../../components/forms/search-client.form";
+import { listAllClients } from "@/actions/clients/list.action";
 
-export default function ClientsPage() {
+import { loadClientsSearchParams } from "@/lib/nuqs/clients.nuqs.loader";
+import type { SearchParams } from "nuqs/server";
+
+type PageProps = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function ClientsPage({ searchParams }: PageProps) {
+  const { page } = await loadClientsSearchParams(searchParams);
+  const clients = await listAllClients({
+    page,
+  });
+
   return (
-    <main className="w-screen h-screen container p-6 text-sm space-y-4">
-      <header className="flex flex-col items-start justify-between">
-        <h2 className="text-lg font-bold tracking-tight">Lista de clientes</h2>
-        <p className="text-muted-foreground">
-          Visualize a lista de clientes disponiveis para a migração
-        </p>
-      </header>
+    <main className="w-screen h-screen container text-sm">
+      <div className="container p-6 pb-10 overflow-y-auto space-y-4">
+        <header className="flex flex-col items-start justify-between">
+          <h2 className="text-lg font-bold tracking-tight">
+            Lista de clientes
+          </h2>
+          <p className="text-muted-foreground">
+            Visualize a lista de clientes disponiveis para a migração
+          </p>
+        </header>
 
-      <SearchClientForm />
+        <SearchClientForm />
 
-      <section>
-        <WWTClientTable data={[]} />
-      </section>
+        <section>
+          <WWTClientTable data={clients} />
+        </section>
+      </div>
     </main>
   );
 }
