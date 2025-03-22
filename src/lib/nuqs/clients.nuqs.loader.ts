b9ebdeff5e5efa62acbfa0e3e59c8c5cc59d2ea5
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-22 19:11:20 -0300

    chore: lmao i forgot to commit that

--- src/lib/nuqs/clients.nuqs.loader.ts
+++ src/lib/nuqs/clients.nuqs.loader.ts
@@ -1,8 +1,9 @@
-import { parseAsFloat, createLoader } from "nuqs/server";
+import { parseAsFloat, createLoader, parseAsString } from "nuqs/server";
 
 // Describe your search params, and reuse this in useQueryStates / createSerializer:
 export const clientsSearchParams = {
-  page: parseAsFloat.withDefault(1),
+  page: parseAsFloat.withDefault(0),
+  accountName: parseAsString,
 };
 
 export const loadClientsSearchParams = createLoader(clientsSearchParams);
