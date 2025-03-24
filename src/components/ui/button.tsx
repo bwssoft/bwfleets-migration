62cb5a6b3a506c06225cf6f54872dc7bf7fd8b2c
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-03-24 11:13:41 -0300

    feat: new components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/@shared/utils/tw-merge";
+import { useFormStatus } from "react-dom";
+import { LoaderIcon } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center cursor-pointer justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -40,19 +44,44 @@ function Button({
   variant,
   size,
   asChild = false,
+  isLoading,
+  children,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    isLoading?: boolean;
   }) {
   const Comp = asChild ? Slot : "button";
+
+  const { pending } = useFormStatus();
+  const shouldDisplayLoadingFeedback =
+    (pending && props.type === "submit") || isLoading;
+
+  if (asChild) {
+    return (
+      <Comp
+        data-slot="button"
+        className={cn(buttonVariants({ variant, size, className }))}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  }
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      disabled={props.disabled || shouldDisplayLoadingFeedback}
       {...props}
-    />
+    >
+      {shouldDisplayLoadingFeedback && (
+        <LoaderIcon className="w-4 h-4 animate-spin" />
+      )}
+      {children}
+    </Comp>
   );
 }
 
