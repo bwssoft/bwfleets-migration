717376ed946ceaa3546062aa5677bf9732bed42b
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-05-05 17:16:06 -0300

    feat: add general form document type toggle

--- src/view/forms/upsert-bwfleet/general.form.tsx
+++ src/view/forms/upsert-bwfleet/general.form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -8,8 +10,33 @@ import {
 import { Input } from "@/view/components/ui/input";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { InputWithAddons } from "@/view/components/ui/input-with-addon";
+import { Controller, UseFormReturn, useWatch } from "react-hook-form";
+import { BWFleetUpsertClientFormData } from "./upsert-bwfleet.handler";
+import React from "react";
+import { InputWithMask } from "@/view/components/ui/input-with-mask";
 
-export function GeneralForm() {
+interface GeneralFormProps {
+  form: UseFormReturn<BWFleetUpsertClientFormData>;
+}
+
+export function GeneralForm({ form }: GeneralFormProps) {
+  const documentType = useWatch({
+    control: form.control,
+    name: "document_type",
+  });
+
+  const documentTypeMask = React.useMemo(() => {
+    if (documentType === "cpf") {
+      return "___.___.___-__";
+    }
+
+    if (documentType === "cnpj") {
+      return "__.___.___/____-__";
+    }
+
+    return undefined;
+  }, [documentType]);
+
   return (
     <Card>
       <CardHeader>
@@ -22,20 +49,60 @@ export function GeneralForm() {
       <CardContent className="space-y-3">
         <div className="grid grid-cols-2 gap-2">
           <div className="grid grid-cols-2 gap-2">
-            <div className="flex flex-col gap-1 justify-center">
-              <label className="text-sm font-medium">Tipo do documento</label>
-              <ToggleGroup type="single" variant="outline" className="w-full">
-                <ToggleGroupItem value="cpf" className="w-full">
-                  CPF
-                </ToggleGroupItem>
-                <ToggleGroupItem value="cnpj" className="w-full">
-                  CNPJ
-                </ToggleGroupItem>
-              </ToggleGroup>
-            </div>
-            <Input
-              label="Documento da empresa"
-              placeholder="00.000.000/00000-00"
+            <Controller
+              control={form.control}
+              name="document_type"
+              render={({ field }) => (
+                <div className="flex flex-col gap-1 justify-center">
+                  <label className="text-sm font-medium">
+                    Tipo do documento
+                  </label>
+                  <ToggleGroup
+                    type="single"
+                    variant="outline"
+                    className="w-full"
+                    value={field.value}
+                    onValueChange={(value) => {
+                      if (value === field.value) return;
+
+                      field.onChange(value);
+                      form.setValue("document", "");
+                    }}
+                  >
+                    <ToggleGroupItem value="cpf" className="w-full">
+                      CPF
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="cnpj" className="w-full">
+                      CNPJ
+                    </ToggleGroupItem>
+                  </ToggleGroup>
+                </div>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="document"
+              render={({ field }) => (
+                <div className="flex flex-col gap-1 justify-center">
+                  <label className="text-sm font-medium">
+                    Documento do cliente
+                  </label>
+                  <InputWithMask
+                    mask={documentTypeMask}
+                    placeholder={
+                      !documentTypeMask
+                        ? "Selecione o tipo do documento"
+                        : undefined
+                    }
+                    replacement={{ _: /\d/ }}
+                    value={field.value}
+                    onChange={(event) => {
+                      field.onChange(event.target.value);
+                    }}
+                  />
+                </div>
+              )}
             />
           </div>
           <Input label="Nome da empresa" />
