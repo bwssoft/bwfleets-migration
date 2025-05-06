270eeb3b6806c1f1e4f134aefff2115172f8c87e
devitalo97 <dev.italo.souza@gmail.com>
2025-05-05 21:29:38 -0300

    style: modify table style

--- src/view/forms/start-migration.form.tsx
+++ src/view/forms/start-migration.form.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { assignMigrationResponsibility, updateMigrationStatus } from "@/@shared/actions/wwt-client.actions";
+import {
+  assignMigrationResponsibility,
+  updateMigrationStatus,
+} from "@/@shared/actions/wwt-client.actions";
 import { WWTClient } from "@/@shared/interfaces/wwt-client";
 import { authClient } from "@/@shared/lib/better-auth/auth-client";
 import { Button } from "@/view/components/ui/button";
@@ -17,11 +20,17 @@ export function StartMigrationForm({ client }: StartMigrationFormProps) {
   const { data } = authClient.useSession();
 
   async function handleAction() {
+    if (!data) return;
     try {
       const formData = new FormData();
       formData.append("uuid", client.id);
       formData.append("status", "in-progress");
-      await assignMigrationResponsibility({ client_id: client.id, user_id: data?.user.id! })
+      await assignMigrationResponsibility({
+        client_id: client.id,
+        user_id: data.user.id!,
+      });
+      // Pelo o que eu entendi, a função 'assignMigrationResponsibility' já atualiza o status para "in-progress"
+      // e não é mais necessário usar a função 'updateMigrationStatus'
       await updateMigrationStatus(formData);
     } catch (error: any) {
       toast.error("Não foi possível atualizar o status de migração", {
