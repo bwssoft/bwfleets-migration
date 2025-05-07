2850e2ecb334de21ca111a04084eda5c0bdf5b74
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-05-07 10:30:46 -0300

    feat: add postal code info fetch on address form

--- src/view/forms/upsert-bwfleet/address.form.tsx
+++ src/view/forms/upsert-bwfleet/address.form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -15,12 +17,31 @@ import { ComboBox } from "@/view/components/ui/single-select-combobox";
 import { CommandItem } from "@/view/components/ui/command";
 import { CheckIcon } from "lucide-react";
 import { cn } from "@/@shared/utils/tw-merge";
+import { useQueryClient } from "@tanstack/react-query";
+import { findAddressByPostalCode } from "@/@shared/actions/viacep.actions";
 
 interface Props {
   form: UseFormReturn<BWFleetUpsertClientFormData>;
 }
 
 export function AddressForm({ form }: Props) {
+  const queryClient = useQueryClient();
+
+  async function handlePostalCodeChange(postalCode: string) {
+    const countryCode = form.getValues("country.code");
+    if (countryCode && countryCode !== "BR") return;
+
+    const result = await queryClient.fetchQuery({
+      queryKey: ["viacep", postalCode],
+      queryFn: () => findAddressByPostalCode(postalCode),
+    });
+
+    form.setValue("street", result.logradouro);
+    form.setValue("district", result.bairro);
+    form.setValue("city", result.localidade);
+    form.setValue("state", result.uf);
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -31,6 +52,49 @@ export function AddressForm({ form }: Props) {
       </CardHeader>
       <CardContent>
         <div className="grid grid-cols-2 gap-2">
+          <div className="col-span-2">
+            <Controller
+              control={form.control}
+              name="country"
+              render={({ field }) => (
+                <div>
+                  <Label>País</Label>
+                  <ComboBox
+                    placeholder="Selecione um país..."
+                    selectedItem={
+                      field.value
+                        ? `${field.value.name} (${field.value?.code})`
+                        : undefined
+                    }
+                    closeOnSelect
+                  >
+                    {countries.map((country) => {
+                      const isSelected = country.code === field.value?.code;
+
+                      return (
+                        <CommandItem
+                          onSelect={() => field.onChange(country)}
+                          key={country.code}
+                          value={country.code}
+                        >
+                          <CheckIcon
+                            className={cn(
+                              "mr-2 h-4 w-4 transition-opacity",
+                              isSelected ? "opacity-100" : "opacity-0"
+                            )}
+                            aria-hidden="true"
+                          />
+
+                          {country.name}
+                        </CommandItem>
+                      );
+                    })}
+                  </ComboBox>
+                </div>
+              )}
+            />
+          </div>
+
           <Controller
             control={form.control}
             name="cep"
@@ -41,7 +105,14 @@ export function AddressForm({ form }: Props) {
                   mask="_____-___"
                   value={field.value}
                   replacement={{ _: /\d/ }}
-                  onChange={(event) => field.onChange(event.target.value)}
+                  onChange={(event) => {
+                    const _cep = event.target.value;
+                    field.onChange(_cep);
+
+                    if (_cep.length === 9) {
+                      handlePostalCodeChange(_cep);
+                    }
+                  }}
                 />
               </div>
             )}
@@ -51,47 +122,6 @@ export function AddressForm({ form }: Props) {
           <Input label="Bairro" {...form.register("district")} />
           <Input label="Cidade" {...form.register("city")} />
           <Input label="Estado" {...form.register("state")} />
-
-          <Controller
-            control={form.control}
-            name="country"
-            render={({ field }) => (
-              <div>
-                <Label>País</Label>
-                <ComboBox
-                  placeholder="Selecione um país..."
-                  selectedItem={
-                    field.value
-                      ? `${field.value.name} (${field.value?.code})`
-                      : undefined
-                  }
-                  closeOnSelect
-                >
-                  {countries.map((country) => {
-                    const isSelected = country.code === field.value?.code;
-
-                    return (
-                      <CommandItem
-                        onSelect={() => field.onChange(country)}
-                        key={country.code}
-                        value={country.code}
-                      >
-                        <CheckIcon
-                          className={cn(
-                            "mr-2 h-4 w-4 transition-opacity",
-                            isSelected ? "opacity-100" : "opacity-0"
-                          )}
-                          aria-hidden="true"
-                        />
-
-                        {country.name}
-                      </CommandItem>
-                    );
-                  })}
-                </ComboBox>
-              </div>
-            )}
-          />
         </div>
       </CardContent>
     </Card>
