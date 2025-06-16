1288465e5144eddc719f74bd5167d3750f7dabdd
Nathan985 <nathan.rodrigu3s@gmail.com>
2025-06-16 11:04:34 -0300

    feat: remove token when disconect

--- src/view/forms/logout.form.tsx
+++ src/view/forms/logout.form.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { signOut } from "@/@shared/actions/user.actions";
+import LocalStorageAdapter from "@/@shared/provider/bwfleets/@base/LocalStorageAdapter";
 import { Button } from "@/view/components/ui/button";
 import { LogOutIcon } from "lucide-react";
 
 export function LogoutForm() {
   async function handleAction() {
+    LocalStorageAdapter.remove("ACCESS_TOKEN");
     await signOut();
   }
 
