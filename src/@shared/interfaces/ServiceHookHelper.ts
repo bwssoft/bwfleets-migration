f20c319791807380313eacbf572cacdbbd8b374f
devitalo97 <dev.italo.souza@gmail.com>
2025-06-17 14:07:29 -0300

    fix: build

--- src/@shared/interfaces/ServiceHookHelper.ts
+++ src/@shared/interfaces/ServiceHookHelper.ts
@@ -1,57 +1,54 @@
-import { AxiosRequestConfig } from "axios"
+import { AxiosRequestConfig } from "axios";
 
 export interface Method {
-	GET: {
-		url: string
-		config?: AxiosRequestConfig
-		data: object
-		responseType?:
-			| "arraybuffer"
-			| "blob"
-			| "document"
-			| "json"
-			| "text"
-			| "stream"
-			| undefined
-	}
-	DELETE: {
-		url: string
-		config?: AxiosRequestConfig
-		data: object
-	}
-	POST: {
-		url: string
-		data?: object
-		config?: AxiosRequestConfig
-	}
-	PUT: {
-		url: string
-		data?: object
-		config?: AxiosRequestConfig
-	}
-	PATCH: {
-		url: string
-		data?: object
-		config?: AxiosRequestConfig
-	}
+  GET: {
+    url: string;
+    config?: AxiosRequestConfig;
+    data: object;
+    responseType?:
+      | "arraybuffer"
+      | "blob"
+      | "document"
+      | "json"
+      | "text"
+      | "stream"
+      | undefined;
+  };
+  DELETE: {
+    url: string;
+    config?: AxiosRequestConfig;
+    data: object;
+  };
+  POST: {
+    url: string;
+    data?: object;
+    config?: AxiosRequestConfig;
+  };
+  PUT: {
+    url: string;
+    data?: object;
+    config?: AxiosRequestConfig;
+  };
+  PATCH: {
+    url: string;
+    data?: object;
+    config?: AxiosRequestConfig;
+  };
 }
 
 export interface IServiceHookHelperResponse<T> {
-	isError: boolean
-	error?: Array<{
-		context: string
-		message: string
-		path: string
-	}>
-	data: T
-	status?: number
+  isError: boolean;
+  error?: Array<{
+    context: string;
+    message: string;
+    path: string;
+  }>;
+  data: T;
+  status?: number;
 }
 
 export interface IServiceHookHelper {
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
-	serviceHookHelper: <K extends keyof Method>(
-		key: K
-	) => <R extends unknown>(
-		request: Method[K]
-	) => Promise<IServiceHookHelperResponse<R>>
+  serviceHookHelper: <K extends keyof Method>(
+    key: K
+  ) => <R>(request: Method[K]) => Promise<IServiceHookHelperResponse<R>>;
 }
